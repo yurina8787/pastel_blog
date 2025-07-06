60ab6ba0f157d82408de5a0b1f630cc7b0eb5e33
yun <68528867+yurina8787@users.noreply.github.com>
2025-07-06 14:25:38 +0900

    package.jsonとpackage-lock.jsonに依存関係を追加し、vite.config.tsの設定を更新
    Signed-off-by: yun <68528867+yurina8787@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import vitePluginRawMd from './vite-plugin-raw-md'
+import type { UserConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss(), vitePluginRawMd()],
-})
+export default defineConfig(({ mode }) => {
+    const isGithubPages = process.env.GITHUB_PAGES === 'true' || mode === 'production';
+    return {
+        plugins: [react(), tailwindcss(), vitePluginRawMd()],
+        base: isGithubPages ? '/<リポジトリ名>/' : '/', // <リポジトリ名>はご自身のものに
+    } as UserConfig;
+});
